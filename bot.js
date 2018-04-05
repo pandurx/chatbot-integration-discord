4ff315f8d70c3277f11e09c46e1e5f38421c2af7
pandurx <internalfire5@live.com>
2018-04-05 08:11:05 -0700

    new stuff

--- bot.js
+++ bot.js
@@ -1,10 +1,62 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+var Discord = require('discord.io');
+var logger = require('winston');
 
-client.on('message', message => {
-	if (message.content === 'ping') {
-		message.reply('pong');
-	}
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
 });
 
-client.login("NDMxNDY0MTUzNDI1NjQxNTA0.DafIWw.YcBBDH7QEVKhVhBy7UL5yzPqR5s");
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: "NDMxNDY0MTUzNDI1NjQxNTA0.DafIWw.YcBBDH7QEVKhVhBy7UL5yzPqR5s",
+   autorun: true
+});
+
+
+
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+    
+    console.log('i have been reborn again!');
+    bot.setPresence( { 
+        game: 
+            { name: 'you play', type: 3 }
+    });
+    
+    
+    const { Client } = require('pg');
+    
+});
+
+
+
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+                
+                console.log("pong!");
+            break;
+
+            // Just add any case commands if you want to..
+         }
+     }
+});
